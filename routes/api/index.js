4f61d57406b2157706afb95f0d7831f06970bb44
smullock <smullock@hotmail.com>
2023-04-08 19:48:46 +1000

    update index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
-const reactionRoutes = require('./reactionRoutes');
 
 router.use('/apps', thoughtRoutes);
 router.use('/users', userRoutes);
-router.use('/users', reactionRoutes);
 
 module.exports = router;
